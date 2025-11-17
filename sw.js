691e9eb1632cf7614f8bf48f4c82b2186409ca48
chirag baser <63344086+chrgbsr@users.noreply.github.com>
2025-11-17 10:14:36 +0530

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,51 @@
+=const CACHE_NAME = 'routinex-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './assets/chime.wav',
+  './assets/icons/icon-192x192.png', // Add your icon paths here
+  './assets/icons/icon-512x512.png',
+  './assets/icons/icon-maskable-192x192.png',
+  './assets/icons/icon-maskable-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        // No cache hit - fetch from network
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
